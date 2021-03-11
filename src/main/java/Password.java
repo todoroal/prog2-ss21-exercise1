79e9d6f0bb4af082b2bdd90394df2e8d2b2a7328
todoroal <alexander.todorovic@stud.fh-campuswien.ac.at>
2021-03-11 15:15:33 +0100

    -Testmethoden  -PW-Länge  -Min. ein Klein- und Großbuschstabe? -Methode  -Tests zum laufen gebracht

--- src/main/java/Password.java
+++ src/main/java/Password.java
@@ -2,11 +2,9 @@ public class Password {
 
     private String Password;
 
-
     public Password (String pw){
         this.Password = pw;
     }
-
 
     /*  • Das Kennwort muss zwischen 8 und 25 Zeichen lang sein
         • Das Kennwort muss aus Klein- und Großbuchstaben bestehen.
@@ -16,8 +14,28 @@ public class Password {
 
     boolean checkPassword(String pw){
 
-    }
+        //Length between 8 -25 ???
+        if(pw.length()<=25 && pw.length()>=8){
+            //Allgemein praktisch
+            char[] list = pw.toCharArray();
 
+            //Minimum 1 lower and 1 upper Case
+            int upperCase=0, lowCase=0;
+            for(int i=0; i<list.length; i++){
+                if(list[i]>=65 && list[i]<=90){
+                    upperCase++;
+                }
+                if(list[i]>=97 && list[i]<=122){
+                    lowCase++;
+                }
+            }
+            if(upperCase==0 || lowCase==0){
+                return false;
+            }
+            else return true;
+        }
+        else return false;
+    }
 
 
     public String getPassword() {
--- src/test/java/PasswordTest.java
+++ src/test/java/PasswordTest.java
@@ -1,15 +1,62 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordTest {
 
     @Test
-    void testCheckPassword_lengthSc1() {
-        Password pw = new Password("W145cgt&zu/$");
+    void testCheckPassword_Larger8() {
+        Password pw = new Password("aRA34%wersa");
         boolean actual = pw.checkPassword(pw.getPassword());
-
-
+        assertEquals(true, actual);
+    }
+    @Test
+    void testCheckPassword_Smaller8() {
+        Password pw = new Password("as3rSa");
+        boolean actual = pw.checkPassword(pw.getPassword());
+        assertEquals(false, actual);
+    }
+    @Test
+    void testCheckPassword_Larger25() {
+        Password pw = new Password("as34%we3453457ghZgzret235dfh65rtzh456rtz54rsa");
+        boolean actual = pw.checkPassword(pw.getPassword());
+        assertEquals(false, actual);
+    }
+    @Test
+    void testCheckPassword_Smaller25() {
+        Password pw = new Password("wEHsa3456");
+        boolean actual = pw.checkPassword(pw.getPassword());
+        assertTrue(actual);
+    }
+    @Test
+    void testCheckPassword_Length25() {
+        Password pw = new Password("123456a89TZJ/&(4567U");
+        boolean actual = pw.checkPassword(pw.getPassword());
+        assertTrue(actual);
+    }
+    @Test
+    void testCheckPassword_Length8() {
+        Password pw = new Password("wERTrs3456");
+        boolean actual = pw.checkPassword(pw.getPassword());
+        assertTrue(actual);
+    }
+    @Test
+    void testCheckPassword_OnlyLowerCase() {
+        Password pw = new Password("werswertet3421");
+        boolean actual = pw.checkPassword(pw.getPassword());
+        assertFalse(actual);
+    }
+    @Test
+    void testCheckPassword_OnlyBiggerCase() {
+        Password pw = new Password("WERTDFGESDF3456");
+        boolean actual = pw.checkPassword(pw.getPassword());
+        assertFalse(actual);
+    }
+    @Test
+    void testCheckPassword_NoCases() {
+        Password pw = new Password("32&§!/34(/$3456");
+        boolean actual = pw.checkPassword(pw.getPassword());
+        assertFalse(actual);
     }
 
 }
